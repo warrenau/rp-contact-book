1b2abade389d52540b869ccc874849fe427a1830
Austin Warren <warrenau@oregonstate.edu>
2024-09-19 20:39:48 -0700

    followed the instructions for step 5. my add contact dialog window is not populating with anything. will need to troubleshoot more.

--- rpcontacts/model.py
+++ rpcontacts/model.py
@@ -21,3 +21,12 @@ class ContactsModel:
         for columnIndex, header in enumerate(headers):
             tableModel.setHeaderData(columnIndex, Qt.Horizontal, header)
         return tableModel
+    
+    def addContact(self,data):
+        """Add a contact to the database."""
+        rows = self.model.rowCount()
+        self.model.insertRows(rows, 1)
+        for column, field in enumerate(data):
+            self.model.setData(self.model.index(rows,column+1), field)
+        self.model.submitAll()
+        self.model.select()
